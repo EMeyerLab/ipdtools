f29dd658451cbf194e6d4aafe6343cba4ff16102
Guillaume DELEVOYE <delevoye@gmcpc04.ens.fr>
2020-01-29 19:31:34 +0100

    Initial commit

---
+++ ipdtools/launchers/ipdtools.py
@@ -0,0 +1,8 @@
+import ipdtools as it
+
+def main():
+    print('ipdtools --dev')
+    raise RuntimeError("CLI not implemented")
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, Extension, find_packages
+import os
+import sys
+
+setup(
+    name='ipdtools',
+    version='0.1',
+    author='DELEVOYE Guillaume',
+    license=open('LICENSES.txt').read(),
+    packages=find_packages("."),
+    package_data={'ipdrools': ['resources/*.h5']},
+    ext_modules=[Extension('ipdtools/tree_predict', ['ipdtools/tree_predict.c'],
+                           extra_compile_args=["-O3", "-shared", "-std=c99"],
+                           export_symbols=["innerPredict", "innerPredictCtx", "init_native"])],
+    zip_safe=False,
+    install_requires=[
+        'numpy'
+    ],
+    entry_points={'console_scripts': [
+        "ipdtools = ipdtools.launchers.ipdtools:main",
+    ]},
+)
