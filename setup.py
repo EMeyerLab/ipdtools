834ec945347b48ae629c1be2656d9bd8a34e2ebd
delevoye <delevoye.guillaume@gmail.com>
2021-03-15 19:03:24 +0100

    Works with conda skeleton

--- setup.py
+++ setup.py
@@ -6,26 +6,18 @@ import sys
 
 setup(
     name='ipdtools',
-    version='0.21',
-    long_description="PacBio codes isolated and repackaged to work on their in-sillico model",
+    version='0.22',
+    description="PacBio codes isolated and repackaged to work on their in-sillico model",
+    long_description="PacBio codes isolated and repackaged to work on their in-sillico model. See https://github.com/GDelevoye/ipdtools for more details.",
     author='DELEVOYE Guillaume',
     author_email="delevoye.guillaume@gmail.com",
     packages=find_packages("."),
     url="https://github.com/GDelevoye/ipdtools",
     python_requires='>=3.6',
-    package_data={'ipdtools': ['resources/*.h5',"./LICENSE.txt","./README.md"]},
+    package_data={'ipdtools': ['resources/*.h5']},
     ext_modules=[Extension('ipdtools/tree_predict', ['ipdtools/tree_predict.c'],
                            extra_compile_args=["-O3", "-shared", "-std=c99"],
                            export_symbols=["innerPredict", "innerPredictCtx", "init_native"])],
-    install_requires=[
-        'numpy',
-        'joblib',
-        'h5py',
-        'pandas',
-        'tqdm',
-        'h5py',
-        'numpy'
-    ],
     entry_points={'console_scripts': [
         "ipdtools = ipdtools.launchers.ipdtools_launcher:main",
     ]},
