af68c835e1d92cf7a29b308c54ed7294520f5297
delevoye <delevoye.guillaume@gmail.com>
2021-03-15 18:41:22 +0100

    Release PyPi achieved

--- setup.py
+++ setup.py
@@ -6,15 +6,14 @@ import sys
 
 setup(
     name='ipdtools',
-    version='0.2a1',
+    version='0.21',
     long_description="PacBio codes isolated and repackaged to work on their in-sillico model",
     author='DELEVOYE Guillaume',
     author_email="delevoye.guillaume@gmail.com",
-    license=open('LICENSE.txt').read(),
     packages=find_packages("."),
     url="https://github.com/GDelevoye/ipdtools",
     python_requires='>=3.6',
-    package_data={'ipdtools': ['resources/*.h5']},
+    package_data={'ipdtools': ['resources/*.h5',"./LICENSE.txt","./README.md"]},
     ext_modules=[Extension('ipdtools/tree_predict', ['ipdtools/tree_predict.c'],
                            extra_compile_args=["-O3", "-shared", "-std=c99"],
                            export_symbols=["innerPredict", "innerPredictCtx", "init_native"])],
