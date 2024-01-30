dd797e6ebda1c85e478f8c6aeb225cb1c2d01a69
Guillaume Delevoye <delevoye.guillaume@gmail.com>
2024-01-30 09:52:46 +0100

    Removed find_packages(".")

--- setup.py
+++ setup.py
@@ -11,7 +11,6 @@ setup(
     long_description="PacBio codes isolated and repackaged to work on their in-sillico model. See https://github.com/GDelevoye/ipdtools for more details.",
     author='DELEVOYE Guillaume',
     author_email="delevoye.guillaume@gmail.com",
-    packages=find_packages("."),
     url="https://github.com/GDelevoye/ipdtools",
     python_requires='>=3.6',
     packages=["h5py","tqdm","numpy","pandas","joblib"],
